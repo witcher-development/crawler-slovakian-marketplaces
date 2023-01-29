eb4ccffb73908f8249a2b8d1b5cee82f73677737
witcher <roman.web.kuznietsov@gmail.com>
2023-01-30 00:33:19 +0100

    bazos categories tree

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,13 +1,43 @@
-import got from 'got';
+import got, { Response } from 'got';
 import * as $jsdom from 'jsdom';
 
 const jsdom = $jsdom.JSDOM;
 
-got.get('https://mobil.bazos.sk/20/?order=3').then((res) => {
-  const { window } = new jsdom(res.body);
-  window.document
-    .querySelectorAll('.inzeratycena b')
-    .forEach((e) => console.log(e.textContent))
+type Category = {
+  link: string;
+  name: string;
+};
+const categoryData = (a: HTMLLinkElement): Category => ({
+  link: a.href,
+  name: a.textContent || '',
 });
 
-console.log('test');
+const getLinksList = (response: Response<any>, selector: string) => {
+  const { window } = new jsdom(response.body);
+  const $ = window.document;
+  return Array.from<HTMLLinkElement>($.querySelectorAll(selector)).map(
+    categoryData,
+  );
+};
+
+got.get('https://www.bazos.sk/').then((res) => {
+  const categories = getLinksList(res, '.nadpisnahlavni a');
+
+  const catsTree: { [key: string]: Category[] } = {};
+
+  categories.forEach((cat, i) => {
+    got.get(cat.link).then((res) => {
+      const subCategories = getLinksList(res, '.barvaleva a');
+      catsTree[cat.name] = subCategories
+        .filter(({ link }) => link.startsWith('/'))
+        .map(({ name, link }) => ({
+          link: cat.link + link.slice(1),
+          name,
+        }));
+
+      if (i === categories.length - 1) {
+        console.log(catsTree);
+      }
+    });
+  });
+});
